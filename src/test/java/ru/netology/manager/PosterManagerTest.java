d6cd8d0131b70aaacdfbd0b2698e9b76b1d7f9d9
Yakushin Alexandr <valensiec08@mail.ru>
2021-12-02 21:07:57 +0300

    commit Movies

--- src/test/java/ru/netology/manager/PosterManagerTest.java
+++ src/test/java/ru/netology/manager/PosterManagerTest.java
@@ -8,19 +8,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PosterManagerTest {
     private PosterManager manager = new PosterManager();
-    private Movie first = new Movie(1,"first","Drama", "www.firstURL.ru");
-    private Movie second = new Movie(2,"second","Comedy", "www.secondURL.ru");
-    private Movie third = new Movie(3,"third","Crime", "www.thirdURL.ru");
-    private Movie fourth = new Movie(4,"fourth","Horror", "www.fourthURL.ru");
-    private Movie fifth = new Movie(5,"fifth","Fantasy", "www.fifthURL.ru");
-    private Movie sixth = new Movie(6,"sixth","War", "www.sixthURL.ru");
-    private Movie seventh = new Movie(7,"seventh","Musical", "www.seventhURL.ru");
-    private Movie eighth = new Movie(8,"eighth","Sci-fi", "www.eighthURL.ru");
-    private Movie ninth = new Movie(9,"ninth","Western", "www.ninthURL.ru");
-    private Movie tenth = new Movie(10,"tenth","Thriller", "www.tenthURL.ru");
-    private Movie eleventh = new Movie(11,"eleventh","biography","www.eleventhURL.ru");
-@BeforeEach
-public void  setUp(){
+    private Movie first = new Movie(1, "first", "Drama", "www.firstURL.ru");
+    private Movie second = new Movie(2, "second", "Comedy", "www.secondURL.ru");
+    private Movie third = new Movie(3, "third", "Crime", "www.thirdURL.ru");
+    private Movie fourth = new Movie(4, "fourth", "Horror", "www.fourthURL.ru");
+    private Movie fifth = new Movie(5, "fifth", "Fantasy", "www.fifthURL.ru");
+    private Movie sixth = new Movie(6, "sixth", "War", "www.sixthURL.ru");
+    private Movie seventh = new Movie(7, "seventh", "Musical", "www.seventhURL.ru");
+    private Movie eighth = new Movie(8, "eighth", "Sci-fi", "www.eighthURL.ru");
+    private Movie ninth = new Movie(9, "ninth", "Western", "www.ninthURL.ru");
+    private Movie tenth = new Movie(10, "tenth", "Thriller", "www.tenthURL.ru");
+    private Movie eleventh = new Movie(11, "eleventh", "biography", "www.eleventhURL.ru");
+
+    @BeforeEach
+    public void setUp() {
         manager.add(first);
         manager.add(second);
         manager.add(third);
@@ -33,7 +34,6 @@ public void  setUp(){
     }
 
 
-
     @Test
     void getTheLastMovieMoreThanOnTheList() {
         Movie[] actual = manager.getLastFilms();
@@ -43,10 +43,10 @@ public void  setUp(){
 
     @Test
     void getTheLastOneLessThanInTheList() {
-    manager.add(tenth);
-    manager.add(eleventh);
-    Movie[] actual =manager.getLastFilms();
-    Movie[] expected = new Movie[]{eleventh,tenth,ninth,eighth,seventh,sixth,fifth,fourth,third,second};
-    assertArrayEquals(expected,actual);
+        manager.add(tenth);
+        manager.add(eleventh);
+        Movie[] actual = manager.getLastFilms();
+        Movie[] expected = new Movie[]{eleventh, tenth, ninth, eighth, seventh, sixth, fifth, fourth, third, second};
+        assertArrayEquals(expected, actual);
     }
 }
