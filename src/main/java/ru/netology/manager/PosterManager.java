f5910837505851e8cc08cd71571532a23fcc4ab8
Yakushin Alexandr <valensiec08@mail.ru>
2021-12-02 21:07:57 +0300

    commit Movies

--- src/main/java/ru/netology/manager/PosterManager.java
+++ src/main/java/ru/netology/manager/PosterManager.java
@@ -4,9 +4,9 @@ import ru.netology.domain.Movie;
 
 public class PosterManager {
     private Movie[] poster = new Movie[0];
-    private int quantityFilm =10;
+    private int quantityFilm = 10;
 
-    public PosterManager(){
+    public PosterManager() {
 
     }
 
@@ -14,22 +14,23 @@ public class PosterManager {
         this.quantityFilm = quantityFilm;
     }
 
-    public void  add (Movie newMovies) {
-        int length = poster.length +1;
+    public void add(Movie newMovies) {
+        int length = poster.length + 1;
         Movie[] newPoster = new Movie[length];
-        for (int i =0; i< poster.length; i++){
-            newPoster [i] = poster[i];
+        for (int i = 0; i < poster.length; i++) {
+            newPoster[i] = poster[i];
         }
         newPoster[newPoster.length - 1] = newMovies;
         poster = newPoster;
 
     }
-    public Movie[] getLastFilms(){
-        int quantity =Math.min(quantityFilm, poster.length);
-        Movie[] result =new Movie[quantity];
-        for (int i=0; i<quantity; i++){
-            int index = poster.length-1-i;
-            result[i]=poster[index];
+
+    public Movie[] getLastFilms() {
+        int quantity = Math.min(quantityFilm, poster.length);
+        Movie[] result = new Movie[quantity];
+        for (int i = 0; i < quantity; i++) {
+            int index = poster.length - 1 - i;
+            result[i] = poster[index];
         }
         return result;
     }
