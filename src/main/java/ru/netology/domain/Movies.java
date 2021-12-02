b7f34ab2a0d1d38eac788925ef21b60f2372ab5e
Yakushin Alexandr <valensiec08@mail.ru>
2021-12-02 15:37:56 +0300

    commit Movies

---
+++ src/main/java/ru/netology/domain/Movies.java
@@ -0,0 +1,11 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Movies {
+    private int id;
+    private String name;
+    private String genre;
+    private String url;
+}
